f28da4207e2ca93c5e8c8a5b715cf659acb65cf2
Milton Munoz <livemilton@gmail.com>
2025-08-02 13:31:26 -0500

    dashboardConfiguration - control Flow @for

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -14,8 +14,12 @@ export default class ControlFlowComponent {
 
   public grade = signal<Grade>('A');
 
+  public frameworks = signal(['Angular','Vue', 'Svelte', 'Qwik', 'React']);
+
+  public frameworks2 = signal([]);
+
   public toggleContent(){
     this.showContent.update(value => !value);
-    
+
   }
 }
