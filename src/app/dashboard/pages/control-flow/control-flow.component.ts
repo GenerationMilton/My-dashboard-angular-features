32e16916ec74aba49dda4a02ffc4e80cddb89b2d
Milton Munoz <livemilton@gmail.com>
2025-08-02 12:28:20 -0500

    dashboardConfiguration - control Flow @if

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-control-flow',
@@ -6,4 +6,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './control-flow.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ControlFlowComponent { }
+export default class ControlFlowComponent { 
+
+  public showContent = signal(false)
+
+  public toggleContent(){
+    this.showContent.update(value => !value);
+  }
+}
