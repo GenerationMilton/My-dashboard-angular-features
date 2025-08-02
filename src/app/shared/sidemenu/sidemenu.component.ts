e4889ccbd62d217959b7eaa4478013aa615d9a0a
Milton Munoz <livemilton@gmail.com>
2025-08-02 12:08:07 -0500

    dashboardConfiguration - @for @if new sintaxis

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { routes } from '../../app.routes';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidemenu',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidemenu.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
