d65dbed553287508dc3db1151d589715045dd552
Milton Munoz <livemilton@gmail.com>
2025-08-02 22:30:30 -0500

    ChangeDetection - Defer Triggers

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeavyLoadersSlowComponent } from '@shared/heavy-loaders/heavy-loaders-slow.component';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-defer-views',
-  imports: [CommonModule, HeavyLoadersSlowComponent],
+  imports: [CommonModule, HeavyLoadersSlowComponent, TitleComponent],
   templateUrl: './defer-views.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class DeferViewsComponent { 
 
-  
+
 }
