5fd71d1f8d15ee872f640b35a677134c1511ddd2
Milton Munoz <livemilton@gmail.com>
2025-08-03 15:33:24 -0500

    ChangeDetection - ViewTransitions part

--- src/app/dashboard/pages/view-transition/view-transition1.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition1.component.ts
@@ -7,16 +7,19 @@ import { TitleComponent } from '@shared/title/title.component';
   imports: [CommonModule, TitleComponent],
   template: ` 
   <app-title title="View Transition 1" />
-  <section class="flex justifi-start">
+  <section class="flex justify-start">
 
     <img srcset="https://picsum.photos/id/237/200/300"
     alt="Picsum"
     width="200"
     height="300"
+    style="view-transition-name: hero1"
     />
 
     <div
-    class="bg-blue-500 w-56 h-56">
+    class="bg-blue-500 w-56 h-56"
+    style="view-transition-name: hero2"
+    >
     </div>
     
   </section>
